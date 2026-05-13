cb4b79d9a0140a05a95c7a18b30c8a01a2f4911c
HamdiHarahap <hamdiharahap2005@gmail.com>
2026-05-13 11:14:37 +0700

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,25 @@ menu.addEventListener('click', () => {
   list.classList.toggle('hidden');
   list.classList.toggle('flex');
 });
+
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  const text = `Halo, saya ingin menghubungi anda.
+Nama: ${name}
+Email: ${email}
+Pesan: ${message}`;
+
+  const phoneNumber = '6281260244154';
+  const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+    text
+  )}`;
+
+  window.open(whatsappURL, '_blank');
+});
